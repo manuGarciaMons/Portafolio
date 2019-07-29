7ff6a1825f8394eafddfc1af08d58296bd728842
Manuels Garcia <mgarciamons@gmail.com>
2019-07-28 20:03:41 -0500

    add imgs

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-const grid = new muuri('.grid', {
+
+const grid = new Muuri('.grid', {
   layout: {
    rounding: false
  }
+});
+
+window.addEventListener('Load', () => {
+  grid.refreshItems().layout();
+  document.getElementById('grid').classList.add('imagenes-cargadas');
 });
