b5c0495675cfd4c41a4453a934e03bf4fb34bdd8
Manuels Garcia <mgarciamons@gmail.com>
2019-07-28 21:57:24 -0500

    document

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+// para las animaciones de las imagenes se utiliza la libreria Muuri
 const grid = new Muuri('.grid', {
 	layout: {
 		rounding: false
@@ -8,7 +9,7 @@ window.addEventListener('load', () => {
 	grid.refreshItems().layout();
 	document.getElementById('grid').classList.add('imagenes-cargadas');
 
-	// Agregamos los listener de los enlaces para filtrar por categoria.
+	// Agregar los listener de los enlaces para filtrar por categoria.
 	const enlaces = document.querySelectorAll('#categorias a');
 	enlaces.forEach((elemento) => {
 		elemento.addEventListener('click', (evento) => {
@@ -21,13 +22,13 @@ window.addEventListener('load', () => {
 		});
 	});
 
-	// Agregamos el listener para la barra de busqueda
+	// Agregar el listener para la barra de busqueda
 	document.querySelector('#barra-busqueda').addEventListener('input', (evento) => {
 		const busqueda = evento.target.value;
 		grid.filter( (item) => item.getElement().dataset.etiquetas.includes(busqueda) );
 	});
 
-	// Agregamos listener para las imagenes
+	// Agregar listener para las imagenes
 	const overlay = document.getElementById('overlay');
 	document.querySelectorAll('.grid .item img').forEach((elemento) => {
 		elemento.addEventListener('click', () => {
